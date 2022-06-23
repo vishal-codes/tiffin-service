d87f8e1a820b0e6295a47e79221105776760031b
vishal-codes <itsvishal2417@gmail.com>
2022-06-23 17:57:09 +0530

    Made a PWA
    Signed-off-by: vishal-codes <itsvishal2417@gmail.com>

---
+++ client/public/serviceWorker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'version-1.0';
+const urlsToCache = ['index.html', 'offline.html'];
+
+const self = this;
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then(async () => {
+            try {
+                return await fetch(event.request);
+            } catch {
+                return await caches.match('offline.html');
+            }
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) =>
+            Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            )
+        )
+    );
+});
